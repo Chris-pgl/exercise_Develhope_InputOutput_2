70c418e7a104663d49d8d3befd9e047bd3eaf4bd
Chris <cr1s@hotmail.it>
2022-12-08 18:33:42 +0100

    Add: exercise_Output2

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+
+        String name = "Chris";
+        String surname = "Puglisi";
+        System.out.printf("MyName is: %s MySurname is: %s ",name,surname);
+    }
+}
